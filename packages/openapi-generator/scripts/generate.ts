1547c7d716d66dfa7ec2f66a89cbfb29f7bd2845
Dániel Földi <dani@stefan.hu>
2024-01-02 21:49:27 +0100

    :zap: Move tmp file to somewhere gitignored so it's not indexed

--- packages/openapi-generator/scripts/generate.ts
+++ packages/openapi-generator/scripts/generate.ts
@@ -7,6 +7,7 @@ import { unstable_dev } from 'wrangler'
 import { attachComments } from './comments'
 import { randomName } from './random'
 import { validate } from './validate'
+import findup from 'findup-sync'
 import type { mainCommand } from './index'
 import type { Paths } from './types'
 import type { CommandDef, ParsedArgs } from 'citty'
@@ -14,8 +15,22 @@ import type { CommandDef, ParsedArgs } from 'citty'
 
 type Args<Type> = Type extends CommandDef<infer X> ? X : never
 
+async function textReplacements(text: string): Promise<string> {
+  // replace /** with /*! to prevent removal of comments
+  text = text.replaceAll('/**', '/*!')
+
+  // make the *server* variable global, so it can be used in the generated code
+  if (/import ?{.*?\bserver\b.*?} ?from '@neoaren\/comet'/s.test(text) && !/durableObject["']?\s*:\s*true/.test(text)) {
+    const serverVariable = text.match(/(const|let) (\S+) ?= ?server\(/s)?.[2]
+    text = `${text}\nglobalThis.worker = ${serverVariable}`
+  }
+
+  return text
+}
+
 export async function generate(args: ParsedArgs<Args<typeof mainCommand>>, data: object) {
-  const tmpFilename = `./tmp-${randomName()}.js`
+  const dir = findup('node_modules') ?? process.cwd()
+  const tmpFilename = `./${dir}/tmp-${randomName()}.js`
   await build({
     entryPoints: [ args.input ],
     bundle: true,
@@ -25,19 +40,21 @@ export async function generate(args: ParsedArgs<Args<typeof mainCommand>>, data:
     format: 'esm',
     plugins: [
       {
-        name: 'replace-jsdoc-with-legal',
+        name: 'text-replacements',
         setup(build) {
           build.onLoad({ filter: /.ts$/ }, async args => {
             const text = await readFile(args.path, 'utf8')
+
             return {
-              contents: text.replaceAll('/**', '/*!'),
+              contents: await textReplacements(text),
               loader: 'ts'
             }
           })
           build.onLoad({ filter: /.js$/ }, async args => {
             const text = await readFile(args.path, 'utf8')
+
             return {
-              contents: text.replaceAll('/**', '/*!'),
+              contents: await textReplacements(text),
               loader: 'js'
             }
           })
@@ -64,6 +81,12 @@ export async function generate(args: ParsedArgs<Args<typeof mainCommand>>, data:
     })
 
     const response = await worker.fetch(`/__generate_openapi__?date=${args.date}`)
+    if (response.headers.get('content-type') !== 'application/json') {
+      console.debug(await response.text())
+
+      throw new Error('An unexpected error has occurred.')
+    }
+
     const paths = await response.json() as Paths
 
     await worker.stop()
@@ -80,6 +103,6 @@ export async function generate(args: ParsedArgs<Args<typeof mainCommand>>, data:
   } catch (error) {
     console.error(error)
   } finally {
-    // await unlink(tmpFilename)
+    await unlink(tmpFilename)
   }
 }
